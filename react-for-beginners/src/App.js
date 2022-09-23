219379132571a682d5a69c21350db7396667c8de
jjjj <dasog94@gmail.com>
2022-09-23 23:08:34 +0900

    useEffect

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,13 +1,35 @@
-import Button from "./Button";
-import styles from "./App.module.css"
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div>
-        <h1 className={styles.title}>Welcome back!</h1>
-        <Button text={"Continue"} />
-    </div>
-  );
+    const [counter, setValue] = useState(0);
+    const [keyword, setKeyword] = useState("");
+    const onClick = () => setValue((prev) => prev + 1);
+    const onChange = (event) => setKeyword(event.target.value);
+    console.log("I run all the time");
+    useEffect(() => {
+        console.log("CALL THE API....");
+    }, []);
+    useEffect(() => {
+        console.log("I run when 'keyword' changes.");
+    }, [keyword]);
+    useEffect(() => {
+        console.log("I run when 'counter' changes.");
+    }, [counter]);
+    useEffect(() => {
+        console.log("I run when 'keyword & counter' changes.");
+    }, [keyword, counter]);
+    useEffect(() => {
+        if (keyword !== "" && keyword.length > 5) {
+            console.log("SEARCH FOR", keyword);
+        }
+    }, [keyword]);
+    return (
+        <div>
+            <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."/>
+            <h1>{counter}</h1>
+            <button onClick={onClick}>click me</button>
+        </div>
+    );
 }
 
 export default App;
