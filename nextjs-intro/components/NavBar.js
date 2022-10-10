301cf9379792ef9ade996d2356ea20b0345f3ec3
jjjj <dasog94@gmail.com>
2022-10-10 21:57:56 +0900

    [NextJS 시작] Patterns / Fetch / Redirect&Rewrite

--- nextjs-intro/components/NavBar.js
+++ nextjs-intro/components/NavBar.js
@@ -1,10 +1,13 @@
 import Link from "next/link";
 import {useRouter} from "next/router";
+import Image from "next/image";
 
 export default function NavBar() {
     const router = useRouter();
     return (
         <nav>
+            {/*<Image src={vercel} alt="vercel" width="283" height="64" />*/}
+            <img src="/vercel.svg"  alt="vercel"/>
             <Link href="/">
                 <a className={router.pathname === "/" ? "active" : ""}>Home</a>
             </Link>
@@ -12,12 +15,34 @@ export default function NavBar() {
                 <a className={router.pathname === "/about" ? "active" : ""}>about</a>
             </Link>
             <style jsx>{`
-              a {
-                text-decoration: none;
+              nav {
+                display: flex;
+                gap: 10px;
+                flex-direction: column;
+                align-items: center;
+                padding-top: 20px;
+                padding-bottom: 10px;
+                box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+                rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+              }
+
+              img {
+                max-width: 100px;
+                margin-bottom: 5px;
+              }
+
+              nav a {
+                font-weight: 600;
+                font-size: 18px;
               }
 
               .active {
                 color: tomato;
+              }
+
+              nav div {
+                display: flex;
+                gap: 10px;
               }
             `}</style>
         </nav>
